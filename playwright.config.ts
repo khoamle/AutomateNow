b206f45c55b71261ad9192601c6b6d9130531c93
Khoa Le <khoaminhle5@gmail.com>
2024-12-18 08:11:26 -0600

    update config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
     baseURL: 'https://practice-automation.com/',
-    headless: false,
+    headless: true,
     browserName: 'chromium',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
