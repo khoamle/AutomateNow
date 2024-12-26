6b01023eb8da17179dc630e827218ce49e6b9e7b
Khoa Le <khoaminhle5@gmail.com>
2024-12-26 11:44:30 -0600

    add more users

--- tests/sauceDemo.spec.ts
+++ tests/sauceDemo.spec.ts
@@ -1,14 +1,28 @@
 import { test, expect, Page } from '@playwright/test';
 import { LoginPage } from '../pages/login.page';
 
-const username = process.env.STANDARD_USER as string
+const standardUsername = process.env.STANDARD_USER as string
+const lockedOutUsername = process.env.LOCKED_OUT_USER as string
+const problemUsername = process.env.PROBLEM_USER as string
 const password = process.env.PASSWORD as string
 
 
-test('Login',async ({ page }) => {
+test('Login as standard user',async ({ page }) => {
     const loginPage = new LoginPage(page)
+    console.log(standardUsername)
     await loginPage.goto()
-    await loginPage.loginToAccount(username, password)
+    await loginPage.loginToAccount(standardUsername, password)
 });
 
+test('Login as lockedOut user',async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    await loginPage.goto()
+    await loginPage.loginToAccount(lockedOutUsername, password)
+});
+
+test('Login as problem user',async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    await loginPage.goto()
+    await loginPage.loginToAccount(problemUsername, password)
+});
 
